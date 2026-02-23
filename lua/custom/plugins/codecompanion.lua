70c1dc1bdabc95889df5cfab1d53cbf91da5e93f
Adnaan Juma <105602441+AimanDebug@users.noreply.github.com>
2026-02-23 11:40:28 +0100

    Added keybindings for codecompanion plugin

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -5,5 +5,28 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
+    'OXY2DEV/markview.nvim',
   },
+  lazy = false,
+  keys = {
+    {
+      '<C-a>',
+      '<cmd>CodeCompanionActions<cr>',
+      mode = { 'n', 'v' },
+      desc = 'Trigger Code Companion Actions',
+    },
+    {
+      '<LocalLeader>a',
+      '<cmd>CodeCompanionChat Toggle<cr>',
+      mode = { 'n', 'v' },
+      desc = 'Toggle Code Companion Chat',
+    },
+    {
+      'ga',
+      '<cmd>CodeCompanionChat Add<cr>',
+      mode = 'v',
+      desc = 'Add selection to Code Companion Chat',
+    },
+  },
+  init = function() vim.cmd [[cab cc CodeCompanion]] end,
 }
