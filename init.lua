30990df650ff4bc914074c567cfb6db999280f10
AimanDebug <105602441+AimanDebug@users.noreply.github.com>
2026-06-02 22:18:24 +0200

    refactor: changed format buffer key

--- init.lua
+++ init.lua
@@ -685,7 +685,7 @@ require('lazy').setup({
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>ff',
         function() require('conform').format { async = true, lsp_format = 'fallback' } end,
         mode = '',
         desc = '[F]ormat buffer',
